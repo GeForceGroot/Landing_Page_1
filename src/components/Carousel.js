0ef299e993d3afa75aa6e3b523b3f552ec1b4e87
Shreyansh Tiwari <shreyansh.tiwari@centralogic.net>
2024-08-15 10:29:23 +0530

    commit-new-5

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -73,65 +73,67 @@ const Carousel = () => {
               </div>
               <div className="col-md-8">
                 <ul className="nav justify-content-center">
-                  <li className="nav-item">
+                  <li className="nav-item" data-aos="fade-down" data-aos-duration="1000" data-aos-once="false" data-aos-mirror="true">
                     <a className="nav-link" href="/" style={{ color: scrolled ? '#015c91' : 'white', fontWeight: '500', fontSize: '16px' }}>Home</a>
                   </li>
-                  <li className="nav-item">
+                  <li className="nav-item" data-aos="fade-down" data-aos-duration="1000" data-aos-once="false" data-aos-mirror="true">
                     <a className="nav-link" href="https://www.ascentwealth.in/#feature-22" target="_blank" style={{ color: scrolled ? '#015c91' : 'white', fontWeight: '500', fontSize: '16px' }}>
                       About&nbsp;Us
                     </a>
                   </li>
-                  <li className="nav-item">
+                  <li className="nav-item" data-aos="fade-down" data-aos-duration="1000" data-aos-once="false" data-aos-mirror="true">
                     <a className="nav-link" href="https://www.ascentwealth.in/#title-7" target="_blank" style={{ color: scrolled ? '#015c91 ' : 'white', fontWeight: '500', fontSize: '16px' }}>
                       Services
                     </a>
                   </li>
                 </ul>
               </div>
-              <div className="col-md-2 text-end">
+              <div className="col-md-2 text-end" data-aos="fade-left" data-aos-duration="1200" data-aos-once="false" data-aos-mirror="true">
                 <button id="btnHvn"><span>Login</span></button>
               </div>
             </div>
           </div>
         </section>
-        <div className="container" style={{ marginTop: "150px", paddingBottom:"118px" }}>
+
+        <div className="container" style={{ marginTop: "150px", paddingBottom: "118px" }}>
           <div className="row">
             <div className="col">
-              <div className="innerBox" style={{ marginTop: '75px', color: '#b9def4', fontSize: '18px', borderRadius: '4px', padding: '8px 14px', background: 'rgb(182 225 248 / 10%)' }}>
+              <div className="innerBox" data-aos="fade-right" data-aos-duration="1000" data-aos-once="false" data-aos-mirror="true" style={{ marginTop: '75px', color: '#b9def4', fontSize: '18px', borderRadius: '4px', padding: '8px 14px', background: 'rgb(182 225 248 / 10%)' }}>
                 Maximize Your Earnings with Expert Tax and Bookkeeping Solutions
               </div>
-              <p style={{ marginTop: "25px", fontSize: '45px', fontWeight: "600", fontFamily: 'ui-sans-serif', color: 'whitesmoke' }}>"Your Quick Way To Success"</p>
-              <p style={{ fontSize: '20px', color: "whitesmoke", textAlign: 'left', marginLeft: "60px", fontWeight: '500', textShadow: "4px 4px 5px rgba(0, 0, 0, 0.1)" }}>
-                A quick way to handle your unique and large operations. Whether it is to manage performance, have immediate tactics, seek AI technologies, recording transactions, paying employees, etc. Have us beside you.
+              <p data-aos="fade-right" data-aos-duration="1200" data-aos-once="false" data-aos-mirror="true" style={{ marginTop: "25px", fontSize: '45px', fontWeight: "600", fontFamily: 'ui-sans-serif', color: 'whitesmoke' }}>"Your Quick Way To Success"</p>
+              <p data-aos="fade-right" data-aos-duration="1200" data-aos-once="false" data-aos-mirror="true" style={{ fontSize: '20px', color: "whitesmoke", textAlign: 'left', marginLeft: "60px", fontWeight: '500', textShadow: "4px 4px 5px rgba(0, 0, 0, 0.1)" }}>
+                A quick way to handle your unique and large operations. Whether it is to manage performance, have immediate tactics, seek AI technologies, recording transactions, paying employees, etc. Have us beside you.
               </p>
             </div>
             <div className="col">
-              <form ref={form} className="form" onSubmit={sendEmail}>
+              <form ref={form} className="form" onSubmit={sendEmail} data-aos="fade-left" data-aos-duration="1200" data-aos-once="false" data-aos-mirror="true">
                 <div className='row'>
                   <h4 style={{ display: 'flex', color: 'rgba(32, 64, 99, 0.8)', fontWeight: '700', textShadow: '4px 4px 5px rgba(0, 0, 0, 0.3)', letterSpacing: '1px' }}> Request a free consult now!</h4>
-                  <div className="col form-group">
-                    <label htmlFor="name" style={{ color: 'whitesmoke', display: 'flex', fontSize:'15px' }}>Your Name (Required)</label>
+                  <div className="col form-group" data-aos="fade-left" data-aos-duration="1000" data-aos-once="false" data-aos-mirror="true">
+                    <label htmlFor="name" style={{ color: 'whitesmoke', display: 'flex', fontSize: '15px' }}>Your Name (Required)</label>
                     <input type="text" id="name" name="name" placeholder="Enter your name" required />
                   </div>
-                  <div className="col form-group">
-                    <label htmlFor="email" style={{ color: 'whitesmoke', display: 'flex', fontSize:'15px' }}>Your Email (Required)</label>
+                  <div className="col form-group" data-aos="fade-left" data-aos-duration="1000" data-aos-once="false" data-aos-mirror="true">
+                    <label htmlFor="email" style={{ color: 'whitesmoke', display: 'flex', fontSize: '15px' }}>Your Email (Required)</label>
                     <input type="email" id="email" name="email" placeholder="Enter your email" required />
                   </div>
                 </div>
-                <div className="form-group" style={{marginTop:'20px'}}>
-                  <label htmlFor="phone" style={{ color: 'whitesmoke', display: 'flex', fontSize:'15px' }}>Phone (Optional)</label>
+                <div className="form-group" data-aos="fade-left" data-aos-duration="1000" data-aos-once="false" data-aos-mirror="true" style={{ marginTop: '20px' }}>
+                  <label htmlFor="phone" style={{ color: 'whitesmoke', display: 'flex', fontSize: '15px' }}>Phone (Optional)</label>
                   <input type="tel" id="phone" name="phone" placeholder="Enter your phone number" />
                 </div>
-                <div className="form-group" style={{marginTop:'20px'}}>
-                  <label htmlFor="requirement" style={{ color: 'whitesmoke', display: 'flex', fontSize:'15px' }}>Tell Us About Your Requirement</label>
+                <div className="form-group" data-aos="fade-left" data-aos-duration="1000" data-aos-once="false" data-aos-mirror="true" style={{ marginTop: '20px' }}>
+                  <label htmlFor="requirement" style={{ color: 'whitesmoke', display: 'flex', fontSize: '15px' }}>Tell Us About Your Requirement</label>
                   <textarea id="requirement" name="requirement" rows="4" placeholder="Describe your needs in detail"></textarea>
                 </div>
-                <button data-aos="fade-left" data-aos-duration="1200" id="btnSubmit"><span>Contact Us</span></button>
+                <button data-aos="fade-left" data-aos-duration="1200" data-aos-once="false" data-aos-mirror="true" id="btnSubmit"><span>Contact Us</span></button>
               </form>
             </div>
           </div>
         </div>
       </section>
+
     </>
   );
 };
