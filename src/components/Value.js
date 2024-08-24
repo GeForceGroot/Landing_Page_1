04c964dc45c1f941fdb208d78e151d0b74c91433
Shreyansh Tiwari <shreyansh.tiwari@centralogic.net>
2024-08-24 23:43:57 +0530

    commit-all-6

--- src/components/Value.js
+++ src/components/Value.js
@@ -10,7 +10,7 @@ const Value = () => {
             <div className="servicebenefites11-section-area section-padding5">
                 <div className="container">
                     <div className="row align-items-center" >
-                        <div id='giveTopMargin' className="col-lg-5 aos-init" data-aos="fade-left" data-aos-duration="800" data-aos-easing="linear" style={{ textAlign: 'start', marginTop:'25px' }}>
+                        <div id='giveTopMargin' className="col-lg-5 aos-init" data-aos="fade-left" data-aos-duration="800" data-aos-easing="linear" style={{ textAlign: 'start', marginTop: '25px' }}>
                             <span className="font-outfit font-15 lineh-16 weight-500 color-h11 aropaln2 d-inline-block margin-b24">Business Model</span>
                             <h1 className="font-outfit font-40 lineh-52 weight-600 color text-capitalize margin-b20 aos-init" data-aos="fade-right" data-aos-duration="1000" data-aos-easing="linear" style={{ textShadow: '4px 4px 5px rgba(0, 0, 0, 0.3)', color: 'rgba(32, 64, 99, 0.8)', marginTop: "10px", fontFamily: 'inherit' }}>Value That Matters</h1>
                             <p className="font-outfit font-15 lineh-26 weight-400 color-21 margin-b">Our business model revolves around our clients, and we plan around their needs, our primary aim is to make it quick while maintaining quality.</p>
@@ -68,19 +68,6 @@ const Value = () => {
                                         </div>
                                     </div>
 
-                                    <div className="col-lg-6 aos-init" id='setValue' data-aos="zoom-in" data-aos-duration="1200">
-                                        <div className="row immigrationarea">
-                                            <div className='col-2'>
-                                                <span className="ts"><div className="author0mages"> <img src={cehck} alt="" className="ts" /></div></span>
-                                            </div>
-                                            <div className='col-10'>
-                                                <p className="font-15 font-outfit lineh-16 weight-600 color ts" id='changeColor'><span className='giveMarg'>Improves annual efficiency by<br/> 11%
-                                                </span></p>
-                                            </div>
-                                        </div>
-                                    </div>
-
-
 
                                     <div className="col-lg-6 aos-init" id='setValue' data-aos="zoom-out" data-aos-duration="1000">
                                         <div className="row immigrationarea">
@@ -94,50 +81,13 @@ const Value = () => {
                                         </div>
                                     </div>
 
-
-
                                     <div className="col-lg-6 aos-init" id='setValue' data-aos="zoom-out" data-aos-duration="1400">
                                         <div className="row immigrationarea">
                                             <div className='col-2'>
                                                 <span className="ts"><div className="author0mages"> <img src={cehck} alt="" className="ts" /></div></span>
                                             </div>
                                             <div className='col-10'>
-                                                <p className="font-15 font-outfit lineh-16 weight-600 color ts" id='changeColor'><span className='giveMarg'>7-layer security tool for in-cloud storage
-                                                </span></p>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div className="col-lg-6 aos-init" id='setValue' data-aos="zoom-out" data-aos-duration="1400">
-                                        <div className="row immigrationarea">
-                                            <div className='col-2'>
-                                                <span className="ts"><div className="author0mages"> <img src={cehck} alt="" className="ts" /></div></span>
-                                            </div>
-                                            <div className='col-10'>
-                                                <p className="font-15 font-outfit lineh-16 weight-600 color ts" id='changeColor'><span className='giveMarg'>Professionals with 14 years<br/> of experience
-                                                </span></p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-6 aos-init" id='setValue' data-aos="zoom-out" data-aos-duration="1400">
-                                        <div className="row immigrationarea">
-                                            <div className='col-2'>
-                                                <span className="ts"><div className="author0mages"> <img src={cehck} alt="" className="ts" /></div></span>
-                                            </div>
-                                            <div className='col-10'>
-                                                <p className="font-15 font-outfit lineh-16 weight-600 color ts" id='changeColor'><span className='giveMarg'>Personalized support whole year
-                                                </span></p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="col-lg-6 aos-init" id='setValue' data-aos="zoom-out" data-aos-duration="800">
-                                        <div className="row immigrationarea">
-                                            <div className='col-2'>
-                                                <span className="ts"><div className="author0mages"> <img src={cehck} alt="" className="ts" /></div></span>
-                                            </div>
-                                            <div className='col-10'>
-                                                <p className="font-15 font-outfit lineh-16 weight-600 color ts" id='changeColor'><span className='giveMarg'>Leverage costly premium Al technologies
-                                                </span></p>
+                                                <p className="font-15 font-outfit lineh-16 weight-600 color ts" id='changeColor'><span className='giveMarg'>Personalized support whole year</span></p>
                                             </div>
                                         </div>
                                     </div>
